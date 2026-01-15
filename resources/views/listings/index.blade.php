78723a5cd791985abdc05a69dcdbe462ea0a1eb8
rhioe <rhioe46@gmail.com>
2026-01-15 23:16:15 +0800

    pre-cleanup-backup

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -1,10 +1,10 @@
-{{-- resources\views\listings\index.blade.php --}}
-@extends('layouts.master')
+    {{-- resources\views\listings\index.blade.php --}}
+    @extends('layouts.master')
 
-@section('title', 'Semua Vendor - WeddingKita')
+    @section('title', 'Semua Vendor - WeddingKita')
 
-@section('content')
-<div class="py-8">
-    @livewire('search.search-listings')
-</div>
-@endsection
+    @section('content')
+    <div class="py-8">
+        @livewire('search.search-listings')
+    </div>
+    @endsection
